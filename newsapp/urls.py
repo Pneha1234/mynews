141b7abd943135cae45abfca6b6950d7690521cb
Pneha1234 <nehapandey408@gmail.com>
2019-06-13 16:09:18 +0545

    django admin template added

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,9 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.contrib.auth import views as auth_views
+from newsapp.views import *
+import django.contrib
 from django.urls import path
-from .views import *
-
+from django.views.generic.base import TemplateView
 
 app_name = "newsapp"
 urlpatterns = [
+	path('', TemplateView.as_view(template_name='admintemplates/editorhome.html'), name='home'), # new
+
+	
 
 
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import *
+
+from django.views.generic.edit import *
+
+from .models import *
+from django.shortcuts import get_object_or_404
+
+
+class NewsCategoryList(ListView):
+    template_name = 'admintemplates/editorhome.html'
+    model = NewsCategory
+    context_object_name = 'newscategory'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['newslist'] = News.objects.all()
+        return context
+
 
 # Create your views here.
